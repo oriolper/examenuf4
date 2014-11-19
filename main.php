b0135e2e34ba14e3c7a023ed140376113693d900
JuanMuFe <juanmf1894@gmail.com>
2014-11-19 11:08:37 +0100

    Commit amb main.php modificat (Ex.8)

--- main.php
+++ main.php
@@ -3,7 +3,10 @@
  * @Author: Admin Project A
  * 
  * */
-
+/*
+ * @Date Creation: 07/11/2014
+ * 
+ * */
 include("resizeimage.php");
 
 function generate_image_thumbnail("cow.jpw","cow-thumb.jpg");
